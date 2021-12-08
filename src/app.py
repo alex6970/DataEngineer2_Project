0aa19eb646ffcfd536e8f72cacbce81553f44044
mulysa99 <44730888+mulysa99@users.noreply.github.com>
2021-12-08 14:21:39 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,11 @@
-from flask import Flask
-import joblib
+import nltk
+from flask import request
+from flask import jsonify
+from flask import Flask, render_template
+
+
+
+app = Flask(__name__)
 
 # model = joblib.load("C://Users/alexa/Desktop/dataengineering2_project/models/lr_model.pkl")
 model = joblib.load("../models/lr_model.pkl")
